05b6166fa05a7cb05de1d127b57200c3c95b7bd9
Vasu Gupta <82724678+vasu3112000@users.noreply.github.com>
2021-07-02 16:53:16 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -42,8 +42,8 @@ def sendEmail(to, content):
     server=smtplib.SMTP('smtp.gmail.com',587)
     server.ehlo()
     server.starttls()
-    server.login('gaarav31100@gmail.com','pardeep69')
-    server.sendmail('gaarav31100@gmail.com',to,content)
+    server.login('abc@gmail.com','abc123')
+    server.sendmail('abc@gmail.com',to,content)
     server.close()
 if __name__ == "__main__":
     wishme()
